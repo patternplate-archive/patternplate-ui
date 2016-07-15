4ec55636a925b5c0a625a1ef7f7e2afed6d6e0e0
dangoo <daniel.gooss@sinnerschrader.com>
2016-07-15 17:45:29 +0200

    feat(organisms/section): Added Section component

---
+++ patterns/organisms/section/index.jsx
@@ -0,0 +1,34 @@
+import React, {PropTypes} from 'react';
+import joinClassnames from 'classnames';
+
+const Section = function (props = {}) {
+	const Component = props.tagName;
+	const componentClassName = 'section';
+	const className = joinClassnames(
+		props.className,
+		componentClassName,
+		`${componentClassName}--${props.type}`
+	);
+
+	return (
+		<Component
+			className={className}
+		>
+			{props.children}
+		</Component>
+	);
+};
+
+Section.propTypes = {
+	className: PropTypes.string,
+	type: PropTypes.string,
+	tagName: PropTypes.node,
+	children: PropTypes.element
+};
+
+Section.defaultProps = {
+	type: 'default',
+	tagName: 'section'
+};
+
+export default Section;
