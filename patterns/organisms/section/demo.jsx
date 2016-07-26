915f486f6b74f9fea70e6ad338da76df374db3dd
Daniel Gooß <daniel.gooss@sinnerschrader.com>
2016-07-26 17:38:43 +0200

    Feat/organisms/section (#2)
    * feat(organisms/section): Added Section component

---
+++ patterns/organisms/section/demo.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable react/react-in-jsx-scope,no-unused-expressions,semi */
+
+import * as React from 'react';
+import Pattern from 'Pattern';
+
+<div>
+	<Pattern/>
+	<Pattern type="highlight"/>
+	<Pattern tagName="footer" type="secondary"/>
+</div>
