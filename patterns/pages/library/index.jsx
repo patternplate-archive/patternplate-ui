d852c64f602d7484baecc9bebfb47cb2888325e6
dangoo <daniel.gooss@sinnerschrader.com>
2016-09-23 20:40:04 +0200

    feat(pages): Added library page

---
+++ patterns/pages/library/index.jsx
@@ -0,0 +1,8 @@
+import LibraryGrid from 'library-grid';
+import MainNavigation from 'main-navigation';
+
+const mainNav = (<MainNavigation pageTitle="patternplate"/>);
+
+<LibraryGrid sidebar={mainNav}>
+	<p>Hallo Welt!</p>
+</LibraryGrid>
